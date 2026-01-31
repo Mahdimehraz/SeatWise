2877fa8ab03cbd874b2ea0357d1a5f0bfaa6a2de
Mahdimehraz <96816398+Mahdimehraz@users.noreply.github.com>
2026-01-31 12:58:19 +0330

    [IMP] notification service : changed the text for email

--- notification-service/index.js
+++ notification-service/index.js
@@ -1,41 +1,53 @@
-require('dotenv').config();
-const { Kafka } = require('kafkajs');
-const nodemailer = require('nodemailer');
+require("dotenv").config();
+const { Kafka } = require("kafkajs");
+const nodemailer = require("nodemailer");
 
 const kafka = new Kafka({
-  clientId: 'notification-service',
-  brokers: [process.env.KAFKA_BROKER]
+  clientId: "notification-service",
+  brokers: [process.env.KAFKA_BROKER],
 });
 
-const consumer = kafka.consumer({ groupId: 'notification-group' });
+const consumer = kafka.consumer({ groupId: "notification-group" });
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 (async () => {
   await consumer.connect();
-  await consumer.subscribe({ topic: 'payment-events', fromBeginning: true });
+  await consumer.subscribe({ topic: "payment-events", fromBeginning: true });
 
   await consumer.run({
     eachMessage: async ({ message }) => {
       const event = JSON.parse(message.value.toString());
 
-      if (event.eventType === 'PAYMENT_COMPLETED') {
+      if (event.eventType === "PAYMENT_COMPLETED") {
         await transporter.sendMail({
           from: process.env.EMAIL_USER,
           to: event.email,
-          subject: 'Payment Successful',
-          text: `Your payment for order ${event.orderId} was successful. Amount: ${event.amount}`
+          subject: "پرداخت با موفقیت انجام شد ✅",
+          text: `
+        سلام
+        
+        پرداخت سفارش شما با موفقیت انجام شد.
+        
+        🧾 شماره سفارش: ${event.orderId}
+        💰 مبلغ پرداختی: ${event.amount}
+        
+        از خرید شما متشکریم 🙏
+        در صورت داشتن هرگونه سوال، پشتیبانی ما همیشه در کنار شماست.
+        
+        با احترام  
+        تیم پشتیبانی
+        `,
         });
-        console.log('Email sent to', event.email);
       }
-    }
+    },
   });
 
-  console.log('Notification service listening for events...');
+  console.log("Notification service listening for events...");
 })();
